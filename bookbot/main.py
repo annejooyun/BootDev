1841c7b362ae2084ccbbcbf672ccc3e17ee24509
Anne Joo Skogseth <ajskogseth@gmail.com>
2026-04-01 13:42:32 +0200

    impl first logic and added file

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return(file_contents)
+
+def count_words(file_string):
+    file_arr = file_string.split()
+    return len(file_arr)
+
+def main():
+    frankenstein_fp = "./books/frankenstein.txt"
+    frankenstein = get_book_text(frankenstein_fp)
+    num_words = count_words(frankenstein)
+    print(f"Found {num_words} total words")
+
+
+main()
